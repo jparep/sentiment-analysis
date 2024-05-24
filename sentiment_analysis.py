5f9ed518d83be22a0a4c1df1b6b463d7485ae99f
jparep <parepson@gmail.com>
2024-05-23 19:23:41 -0600

    sentiment_analysis.py file added

---
+++ sentiment_analysis.py
@@ -0,0 +1,22 @@
+#Importing all the neccessary libraries
+import pandas as pd
+import re
+from bs4 import BeautifulSoup
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import CountVectorizer
+import seaborn as sns
+from nltk.stem import WordNetLemmatizer
+import nltk
+nltk.download('stopwords') # Download the stopwords dataset
+from nltk.corpus import stopwords
+from sklearnex import patch_sklearn 
+patch_sklearn()
+
+lem = WordNetLemmatizer()
+stopWords = set(stopwords.words("english"))
+
+data = pd.read_csv('IMDB_dataset.csv')
+print(data)
+
