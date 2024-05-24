c32cae824964e1c1a3669522dd245042b885338f
jparep <parepson@gmail.com>
2024-05-23 22:44:14 -0600

    vectorizer added

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -9,7 +9,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 import seaborn as sns
 from nltk.stem import WordNetLemmatizer
 import nltk
-nltk.download('stopwords')  # Download the stopwords dataset
+#nltk.download('stopwords')  # Download the stopwords dataset
 from nltk.corpus import stopwords
 from sklearnex import patch_sklearn 
 patch_sklearn()
@@ -55,3 +55,7 @@ target = df["sentiment"]
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(feature, target, test_size=0.2, random_state=121)
+
+vectorizer = CountVectorizer()
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
